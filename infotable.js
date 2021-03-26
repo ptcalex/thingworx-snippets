a0845b652a3ebbb6dd988d2dd73c3ab289c683f0
Alessio Marchetti <amarchetti@ptc.com>
2021-03-26 23:34:05 +0100

    add iterate snippet

--- infotable.js
+++ infotable.js
@@ -2,7 +2,6 @@
 /*** CREATE INFOTABLE ***/
 
 // Name:STRING (name of DATASHAPE with fields field1 and field2)
-
 let infotable = DataShapes[Name].CreateValues();
 infotable.AddRow({ field1: value1, field2: value2 })
 
