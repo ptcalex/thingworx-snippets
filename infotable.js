d5fdca3b84853727a99187a837f87a393524e824
Alessio Marchetti <amarchetti@ptc.com>
2021-03-26 23:19:10 +0100

    added

---
+++ infotable.js
@@ -0,0 +1,8 @@
+/*** CREATE INFOTABLES ***/
+
+// Name:STRING (name of DATASHAPE with fields field1 and field2)
+
+let infotable = DataShapes[Name].CreateValues();
+infotable.AddRow({ field1: value1, field2: value2 })
+
+
