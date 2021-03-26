63b7ffe6077f625ee6d64c02f35b1335de8e37ed
Alessio Marchetti <amarchetti@ptc.com>
2021-03-26 23:32:25 +0100

    iterate infotable

--- infotable.js
+++ infotable.js
@@ -7,3 +7,11 @@ let infotable = DataShapes[Name].CreateValues();
 infotable.AddRow({ field1: value1, field2: value2 })
 
 
+
+/*** ITERATE INFOTABLE ***/
+
+// Infotable:INFOTABLE
+Infotable.rows.toArray().forEach(obj => {
+  // obj represents a row object
+});
+
