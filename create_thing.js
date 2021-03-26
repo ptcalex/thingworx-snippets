0fccec4d357b7d6311467f24da3300931c7cdda1
Alessio Marchetti <amarchetti@ptc.com>
2021-03-26 09:53:04 +0100

    add create thing snippet

---
+++ create_thing.js
@@ -0,0 +1,22 @@
+/*** CREATE A THING ***/
+
+// Parameters: Name:THINGNAME
+
+if (Things[Name]) { // delete if already existing
+    Resources["EntityServices"].DeleteThing({ name: Name });
+}
+
+Resources["EntityServices"].CreateThing({
+    name: Name,
+    description: "Describe your thing",
+    thingTemplateName: "GenericThing",
+    projectName: "AM.YourProject.PJ"
+});
+    
+let thing = Things[Name];
+thing.EnableThing();
+thing.RestartThing();
+
+
+
+
