7fc34207b6ab1091f9a4f684c5dd8212643967ec
Alessio Marchetti <amarchetti@ptc.com>
2021-03-26 23:35:11 +0100

    fixed typos

--- thing.js
+++ thing.js
@@ -1,22 +1,17 @@
+
 /*** CREATE A THING ***/
 
 // Name:THINGNAME
-
 if (Things[Name]) { // delete if already existing
     Resources["EntityServices"].DeleteThing({ name: Name });
 }
-
 Resources["EntityServices"].CreateThing({
     name: Name,
     description: "Describe your thing",
     thingTemplateName: "GenericThing",
     projectName: "AM.YourProject.PJ"
 });
-    
 let thing = Things[Name];
 thing.EnableThing();
 thing.RestartThing();
-
-
-
 
