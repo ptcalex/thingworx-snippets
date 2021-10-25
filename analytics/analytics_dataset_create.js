91544310f48252b704418d97ed02959fd034a85d
Alessio Marchetti <amarchetti@ptc.com>
2021-10-25 18:43:13 +0200

    dataset creation refactored

--- analytics/analytics_dataset_create.js
+++ analytics/analytics_dataset_create.js
@@ -1,4 +1,5 @@
 /*
+
 TO  create a ThingWorx Analytics dataset
 I   get a dataset reference to a CSV file located in a repository (9.x)
     infer dataset metadata using the dataset reference, and save the metadata to a file in the repository (9.x)
@@ -8,6 +9,7 @@ I   get a dataset reference to a CSV file located in a repository (9.x)
 PARAMETERS: DatasetName (mydataset), DataSetPath (/data.csv), MetadataPath (/metadata.json)
 PROPERTIES: me.DatasetRepository (a FileRepository), me.DataThing (AnalyticsServer_DataThing), me.DatasetJobID
 RETURN:     Job ID
+
 */
 
 // let datasetref = getDatasetReference(DatasetPath); // 9.x
@@ -20,31 +22,31 @@ let result = me.DatasetJobID = jobid;
 // ---------- IMPLEMENTATION DETAILS ---------- //
 
 function getDatasetReference() {
-	let datasetRef = DataShapes.AnalyticsDatasetRef.CreateValues();
-	datasetRef.AddRow({ datasetUri: encodeURI("thingworx://" + me.DatasetRepository + DatasetPath), format: "csv" });
-	return datasetRef;    
+  let datasetRef = DataShapes.AnalyticsDatasetRef.CreateValues();
+  datasetRef.AddRow({ datasetUri: encodeURI("thingworx://" + me.DatasetRepository + DatasetPath), format: "csv" });
+  return datasetRef;    
 }
 
 function inferMetadataFromDatasetRef(datasetRef) {
-	return me.DataThing.DetectMetadataJSON({ datasetRef: datasetRef });
+  return me.DataThing.DetectMetadataJSON({ datasetRef: datasetRef });
 }
 
 function saveMetadata(metadata) {
-	Things[me.DatasetRepository].SaveText({ path: MetadataPath, content: JSON.parse(metadata) });
+  Things[me.DatasetRepository].SaveText({ path: MetadataPath, content: JSON.parse(metadata) });
 }
 
 function getDatasetTags() {
-    let tags = DataShapes.GenericStringList.CreateValues();
-    tags.AddRow({ item: "builderdataset" });
-    tags.AddRow({ item: "builderdatasetname:" + DatasetName });    
-    return tags;
+  let tags = DataShapes.GenericStringList.CreateValues();
+  tags.AddRow({ item: "builderdataset" });
+  tags.AddRow({ item: "builderdatasetname:" + DatasetName });    
+  return tags;
 }
 
 function createDatasetTaggedBy(tags) {
-	return Things[me.DataThing].CreateDataset({
-        csvURI: encodeURI("thingworx://" + me.DatasetRepository + DatasetPath),
-        csvHasHeaders: true,
-        metadataFileURI: encodeURI("thingworx://" + me.DatasetRepository + MetadataPath),
-        tags: tags
-	});
+  return Things[me.DataThing].CreateDataset({
+    csvURI: encodeURI("thingworx://" + me.DatasetRepository + DatasetPath),
+    csvHasHeaders: true,
+    metadataFileURI: encodeURI("thingworx://" + me.DatasetRepository + MetadataPath),
+    tags: tags
+  });
 }
