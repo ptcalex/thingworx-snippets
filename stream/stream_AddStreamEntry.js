99bd1c905017f5d1dc4f0bbf5e6c7d3c57826984
Alessio Marchetti <amarchetti@ptc.com>
2022-05-09 18:54:23 +0200

    Refactored

--- stream/stream_AddStreamEntry.js
+++ stream/stream_AddStreamEntry.js
@@ -1,15 +1,14 @@
 /*** CREATE STREAM ENTRY - SINGLE ENTRY ***/
 
-let stream = ""; // name of Stream entity
+const STREAM_NAME = ""; // name of Stream entity
 
-let values = Things[stream].CreateValues();
-values.AddRow({ /* fields of the Stream's Data Shape here */ });
-
-Things[stream].AddStreamEntry({
-    tags: [], // array of tags where each tag is a string of the kind "Vocabulary:Tag"
-    timestamp: new Date(), // pick a date
-    source: me.name,
-    values: values, // as filled above
-    location: { latitude: 0, longitude: 0, elevation: 0, units: "WGS84" } // pick a location
+const stream = Things[STREAM_NAME];
+const values = stream.CreateValuesWithData({ values: {/* Stream's Data Shape */} });
+stream.AddStreamEntry({
+  timestamp: new Date(), // pick a date
+  values: values, // as filled above
+  source: me.name,
+  tags: [], // array of tags where each tag is a string of the kind "Vocabulary:Tag"
+  location: { latitude: 0, longitude: 0, elevation: 0, units: "WGS84" } // pick a location
 });
 
