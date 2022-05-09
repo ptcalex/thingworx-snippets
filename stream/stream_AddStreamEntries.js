d1782ce1c1ee3166a388f0f8adcd40d043c2ef60
Alessio Marchetti <amarchetti@ptc.com>
2022-05-09 18:50:35 +0200

    Using StreamEntryWithValues Data Shape

--- stream/stream_AddStreamEntries.js
+++ stream/stream_AddStreamEntries.js
@@ -1,31 +1,20 @@
 /*** CREATE STREAM ENTRIES - MULTIPLE ENTRIES ***/
 
-let infotable = getStreamInfotable(); // getStreamInfotable function defined below
-let streamName = "..."; // name of Stream entity
-let stream = Things[streamName];
+const STREAM_NAME = ""; // name of stream entity
+
+const stream = Things[STREAM_NAME];
+const infotable = DataShapes["StreamEntryWithValues"].CreateValues();
+
 while (/* your condition here */) {
-    let values = stream.CreateValues(); // fields of Stream's DataShape
-    values.AddRow({ /* DataShape fields */ });
-    infotable.rows.push({
-        timestamp: new Date(), // pick a date
-        location: { latitude: 0, longitude: 0, elevation: 0, units: "WGS84" }, // pick a location
-        source: me.name,
-        tags: [], // array of tags where each tag is a string of the kind "Vocabulary:Tag"
-        values: values // single-row intofable
-    });
+  const data = stream.CreateValuesWithData({ values: { /* Stream's DataShape */ } });
+  infotable.AddRow({
+    timestamp: new Date(), // pick a date
+    values: data, // single-row intofable
+    source: me.name,
+    tags: [], // array of tags where each tag is a string of the kind "Vocabulary:Tag"
+    location: { latitude: 0, longitude: 0, elevation: 0, units: "WGS84" } // pick a location
+  });
 }
+
 stream.AddStreamEntries({ values: infotable });
-
-
-// supporting function
-function getStreamInfotable() {
-    let infotable = { dataShape: { fieldDefinitions: {} }, rows: [] };
-    infotable.dataShape.fieldDefinitions['timestamp'] = { name: 'timestamp', baseType: 'DATETIME' };
-    infotable.dataShape.fieldDefinitions['location'] = { name: 'location', baseType: 'LOCATION' };
-    infotable.dataShape.fieldDefinitions['source'] = { name: 'source', baseType: 'STRING' };
-    infotable.dataShape.fieldDefinitions['sourceType'] = { name: 'sourceType', baseType: 'STRING' };
-    infotable.dataShape.fieldDefinitions['tags'] = { name: 'tags', baseType: 'TAGS' };
-    infotable.dataShape.fieldDefinitions['values'] = { name: 'values', baseType: 'INFOTABLE' };
-	  return infotable;
-}
 
